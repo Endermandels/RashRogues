877a7d61e6d5db3d51e42093ae22019399c47fc7
CadeTormanen <tormacad000@gmail.com>
2024-10-31 13:51:12 -0700

    create empty libdgx project

---
+++ core/src/main/java/io/github/RashRogues/Main.java
@@ -0,0 +1,27 @@
+package io.github.RashRogues;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+    private SpriteBatch batch;
+    private Texture image;
+
+    @Override
+    public void create() {
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+    }
+
+    @Override
+    public void dispose() {
+    }
+}
