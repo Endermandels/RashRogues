fd969413bc1f0532d613f72c69c8be11a9aa40aa
Endermandels <131790297+Endermandels@users.noreply.github.com>
2024-12-13 09:01:44 -0800

    Fix Merchant font (#159)

--- core/src/main/java/Networking/Server.java
+++ core/src/main/java/Networking/Server.java
@@ -38,7 +38,7 @@ public class Server implements Endpoint{
      */
     public void host(){
         Server server = this; //add server to this block scope, so we can use it inside the thread.
-        primarySocket = Gdx.net.newServerSocket(Network.PROTOCOL,"192.168.11.114",Network.PORT,null);
+        primarySocket = Gdx.net.newServerSocket(Network.PROTOCOL,"127.0.0.1",Network.PORT,null);
         clients = Collections.synchronizedList(new ArrayList<ClientListener>());
         cleanupQueue = new Queue<>();
         heartbeatStatus = new LinkedHashMap();
